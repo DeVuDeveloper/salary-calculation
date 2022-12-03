a27d3a3ac932b8821dc43cec4fe39888d320024b
VuDej <drvu47@gmail.com>
2022-12-03 05:41:17 +0100

    adding sidebar, theme and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
-import "./App.css";
-const App = () => {
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+
+function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
+
   return (
-    <div>
-      <h1 className='test'>Testing</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Box
+        bgcolor={"background.default"}
+        color={"text.primary"}
+        height={"100vh"}
+      >
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+        
+          <Router>
+          <Sidebar mode={mode} setMode={setMode} />
+            <Routes>
+              
+             
+            </Routes>
+          </Router>
+          
+        </Stack>
+        
+      </Box>
+    </ThemeProvider>
   );
-};
+}
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -29,4 +29,5 @@ module.exports = {
       template: "public/index.html",
     }),
   ],
+
 };
