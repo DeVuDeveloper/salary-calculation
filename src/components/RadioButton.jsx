95d6eb271a9ba354aa47c7e4dedb4e6c13d6ec0f
VuDej <drvu47@gmail.com>
2022-12-03 13:34:30 +0100

    color blue set for hovers

--- src/components/RadioButton.jsx
+++ src/components/RadioButton.jsx
@@ -5,7 +5,7 @@ const RadioButton = () => {
   
 
     <div class="main flex border rounded-full overflow-hidden m-4 select-none">
-      <div class="title py-3 my-auto px-5 bg-blue-500 text-white text-sm font-semibold mr-3">Sallary</div>
+      <div class="title py-3 my-auto px-5 bg-blue-400 text-white text-sm font-semibold mr-3">Sallary</div>
       <label class="flex radio p-2 cursor-pointer">
         <input class="my-auto transform scale-125" type="radio" name="sfg" />
         <div class="title px-2">Gross</div>
