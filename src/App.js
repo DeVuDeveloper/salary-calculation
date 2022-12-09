bc60e2f550e927392bfb11d6faf2485aaec125e9
VuDej <drvu47@gmail.com>
2022-12-09 15:19:49 +0100

    linter

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Sidebar from "./components/common/Sidebar";
-import Rightbar from "./components/common/Rightbar";
-import Hamburger from "./components/common/Hamburger";
-import Home from "./pages/Home";
-import NotFound from "./pages/404";
-import Salary from "./pages/Salary";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/common/Sidebar';
+import Rightbar from './components/common/Rightbar';
+import Hamburger from './components/common/Hamburger';
+import Home from './pages/Home';
+import NotFound from './pages/404';
+import Salary from './pages/Salary';
+import { Box, createTheme, Stack, ThemeProvider } from '@mui/material';
+import { useState } from 'react';
 import './app.css';
 
-import { Box, createTheme, Stack, ThemeProvider } from "@mui/material"
-import { useState } from "react"
-
-function App() {
-  const [mode, setMode] = useState("light")
+function App () {
+  const [mode, setMode] = useState('light')
 
   const darkTheme = createTheme({
     palette: {
-      mode: 'dark',
-    },
+      mode: 'dark'
+    }
   })
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <Box bgcolor={"background.default"} color={"text.primary"} height={"100vh"}>
-        <Stack direction="row" spacing={2} justifyContent="space-between">
+      <Box bgcolor={'background.default'} color={'text.primary'} height={'100vh'}>
+        <Stack direction='row' spacing={2} justifyContent='space-between'>
           <Router>
             <Sidebar mode={mode} setMode={setMode} />
             <Hamburger />
             <Routes>
-              
-              <Route path="/" element={<Home />} />
-              <Route path="salary" element={<Salary />} />
-              <Route path="*" element={<NotFound />}/>
+              <Route path='/' element={<Home />} />
+              <Route path='salary' element={<Salary />} />
+              <Route path='*' element={<NotFound />} />
             </Routes>
           </Router>
           <Rightbar />
@@ -41,4 +39,4 @@ function App() {
   )
 }
 
-export default App;
+export default App
